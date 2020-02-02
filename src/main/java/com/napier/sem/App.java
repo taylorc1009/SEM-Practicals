656e1d75689c7d4cb83d378884a0eeef910e1491
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-02-02 19:17:28 +0000

    Connect/disconnect integration + password update

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -45,7 +45,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "password");
                 System.out.println("Successfully connected");
                 break;
             }
